3e756ee1ccee4eb01cd7020c255393e5b8fa8b87
rico5550 <42789157+rico5550@users.noreply.github.com>
2022-09-20 16:58:28 -0700

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,1 +1,24 @@
 #include "function.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+string readme(string filename) {
+    
+    ifstream newfile;
+
+    newfile.open(filename);
+    string line;
+    string txt;
+    while (getline(newfile, line)){
+        txt.append(line);
+        txt.append("\n");
+    }
+
+
+    cout << txt << endl;
+    return txt;
+}
+
