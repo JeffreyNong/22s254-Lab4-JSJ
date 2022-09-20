4e4459c7cd74110f30af3ef4671c0974176a968d
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-09-20 14:38:05 -0700

    Added display function

--- function.cpp
+++ function.cpp
@@ -1,1 +1,8 @@
 #include "function.h"
+#include <iostream>
+using namespace std;
+
+void display(string s)
+{
+  cout << s << endl;
+}
--- function.h
+++ function.h
@@ -1,6 +1,8 @@
+#include <iostream>
+
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-
+void display(std::string s);
 
 #endif
